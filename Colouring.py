5fddfa175cf79a5d48218a95428b541cde0b1fb6
Sir Isaac Newton <marc.chevalier@ens-lyon.org>
2016-05-11 12:08:52 +0200

    Code base

--- Colouring.py
+++ Colouring.py
@@ -1,29 +1,64 @@
+from random import *
+from math import *
+
+
 class Colouring:
     """ All functions related to colouring the graph """
 
-    def __init__(self, graph, q):
+    def __init__(self, graph, q, beta):
         """
         :param graph: Graph to be coloured
         :param q: Total number of colors
         """
-        self.vertices = graph.vertices
-        self.edges = graph.edges
-        self.colors = range(q)
+        self.graph = graph
+        self.q = q
+        self.beta = beta
+        self.t = 0
 
-    def coloring(self, coloring):
+    def init_random_coloring(self):
         """
-        :param coloring: List of tuples (vertex,color)
+        Assign a random coloring to the graph.
         """
-        for (vertex,color) in coloring:
-            vertex.color = color
+        self.graph.coloring = [randrange(self.q) for _ in self.graph.n]
+        self.H = self.hamiltonian()
 
     def hamiltonian(self):
         """
         :return: The Hamiltonian function of the current coloring (we suppose that every vertex has been colored
         """
-        h = 0
-        for (a,b) in self.edges:
-            if a.color == b.color:
-                h += 1
-        return h
+        return \
+            sum(map(
+                    lambda a:
+                    sum(filter(
+                            lambda b: int(self.graph.coloring[a] == self.graph.coloring[b]),
+                            self.graph.adjacency[a])),
+                    range(self.graph.n)))\
+            / 2
 
+    def metropolis(self, n):
+        self.init_random_coloring()
+        H_hist = [self.H]
+        for _ in range(n):
+            self.step()
+            H_hist.append(self.H)
+
+        return H_hist
+
+    def step(self):
+        v = randrange(self.graph.n)
+        old_color = self.graph.coloring[v]
+        new_color = randrange(self.q-1)
+        if new_color >= old_color:
+            new_color += 1
+        delta = 0
+        for u in self.graph.adjacency[v]:
+            if self.graph.coloring[u] == new_color:
+                delta += 1
+            elif self.graph.coloring[u] == old_color:
+                delta -= 1
+
+        if delta <= 0 or random() < exp(-self.beta(self.t)*delta):
+            self.graph.coloring[v] = new_color
+            self.H += delta
+
+        self.t += 1
--- Graph.py
+++ Graph.py
@@ -1,50 +1,33 @@
+from random import *
+
 class Graph:
     """ Graph with vertices that can be colored """
 
-    def __init__(self, vertices=None, edges=None):
-        """
-        :param vertices: List of vertices
-        :param edges: List of edges
-        """
-        if vertices == None:
-            self.vertices = []
-        else:
-            self.vertices = vertices
-
-        if edges == None:
-            self.edges = []
-        else:
-            self.edges = edges
-
-    def add_edge(self, edges):
-        """
-        :param edges: List of edges to be added (tuple of 2 vertices)
-        """
-        for edge in edges:
-            self.edges.append(edge)
-
-    def neighbours(self, vertice):
-        """
-        :param vertice: A vertex to be studied for neighbours
-        :return: List of vertices that share an edge with vertice
-        """
-        neighbours = []
-        for (a,b) in self.edges:
-            if a == vertice:
-                neighbours.append(b)
-            elif b == vertice:
-                neighbours.append(a)
-        return neighbours
+    def __init__(self, n, mat):
+        self.n = n
+        self.adjacency = list(map(lambda row: set([i for i, x in enumerate(row) if x == 1]), mat))
+        self.coloring = [0 for _ in range(n)]
 
     def __repr__(self):
         string = "List of vertices :\n"
-        for vertex in self.vertices:
+        for vertex in range(self.n):
             strv = repr(vertex)
             string += "  " + strv + "\n"
         string += "\n" + "List of edges :\n"
-        for (a,b) in self.edges:
-            stra = repr(a)
-            strb = repr(b)
-            string += "  " + stra + " <--> " + strb + "\n"
+        for a in range(self.n):
+            for b in self.adjacency[a]:
+                stra = repr(a)
+                strb = repr(b)
+                string += "  " + stra + " <--> " + strb + "\n"
         string += "\n"
         return string
+
+
+def erdos_renyi(n, c):
+    mat = [[0 for _ in range(n)] for _ in range(n)]
+    for i in range(n):
+        for j in range(i):
+            r = int(random() < 1.0 * c / n)
+            mat[i][j] = r
+            mat[j][i] = r
+    return Graph(n, mat)
