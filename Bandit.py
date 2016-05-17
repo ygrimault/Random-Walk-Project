6b2acb5c2238dc99975fac985e784232a881b974
George Zakhour <g1995z@gmail.com>
2016-05-17 18:38:26 +0200

    Used Bandits to minimize convergence time over beta(t)
    To train and observe the output execute:
    > make run
    which will create a file called log.txt in which information
    about each learning step.
    
    PS: loss function might need to be changed. And an actual test must
    be ran.

---
+++ Bandit.py
@@ -0,0 +1,146 @@
+from math import factorial, exp, sin
+from scipy import stats
+from Colouring import Colouring
+import CMDPlot
+import sys
+from statistics import median
+
+class Bandit_beta:
+
+    def _lambda_change_ith_cooeficient(self, i, v):
+        '''
+        Generates a lambda that when invoked on a set of cooficients will
+        increment the i-th one by v
+
+        :param i: the i-th cooficient
+        :param v: the additive constant to add to the i-th cooficient
+
+        :return: lambda : \R^{degree+1} -> \R^{degree+1}
+        '''
+        return lambda arr:\
+                [x + int(j == i)*v for j,x in enumerate(arr)]
+
+
+
+    def __init__(self, graph, number_colours, metropolis_iter = 1e5, degree=6, log=True):
+        '''
+        Create the bandit instance with a polynomial of a given degree.
+        There will be 2*degree arms, each will either increase or decrease
+        a cooeficient with a value of 0.1/i! where i is the i-th power
+        coresponding to that cooficient.
+
+        :param graph: graph on which to do the colouring
+        :param number_colours: number of colours to colour the graph
+        :param metropolis_iter: number of iterations to run the metropolis algorithm on
+        :param degree: the degree of the polynomial
+        :param log: whether or not to log the process of learning
+        '''
+        self.graph = graph
+        self.number_colours = number_colours
+        self.metropolis_iter = metropolis_iter
+        self.log = log # Whether or not to log the process
+        self.degree = degree
+        self.cooficients = [1] * (degree+1)
+        self.arms = []
+        self.weights = [1.0] * (2*degree)
+        for i in range(2*degree):
+            v = (-1) ** i * 0.1
+            self.arms.append(self._lambda_change_ith_cooeficient(int(i/2), v))
+
+        self.min_loss = float('inf') # the minimum loss found so far
+        self.min_cooeficients = [] # ^ its corresponding coefficients
+
+
+    def loss(self, j):
+        '''
+        Compute the loss produced by choosing the j-th arm.
+        The loss of an arm is the following: (We can maybe choose a different one)
+        The first time the minimum value is attained / number of iterations.
+
+        :param j: the index of the arm selected
+        :return: l \in [0, 1] (Needed to have a guarantee on the convergence)
+        '''
+        self.cooficients = self.arms[j](self.cooficients)
+        new_beta = self.get_polynomial()
+        losses = []
+        for _ in range(10):
+            c = Colouring(self.graph, int(self.number_colours), new_beta)
+            history = c.metropolis(int(self.metropolis_iter))
+            losses.append(history.index(min(history))/len(history))
+        return median(losses)
+
+
+
+    def step(self):
+        '''
+        Performs one step of the EXP3 algorithm:
+            1. Convert the weights into a distribution
+            2. Acount for the exploration factor in the distribution
+            3. Pick an arm based on this distribution
+            4. Compute the loss produced by this arm and update its weight
+        '''
+        phi = sum(self.weights)
+        n = len(self.weights)
+        # the factor of exploration, i.e. with probability 1/2n it will try new arms
+        eps = (1.0/n)*0.5
+        # distribution of picking arm
+        distribution = list(map(lambda w: (1-n*eps)*w/phi + eps, self.weights))
+        # random variable with the distribution above
+        RV = stats.rv_discrete(values=(list(range(n)), distribution))
+        j = RV.rvs() # pick a random arm
+        loss = self.loss(j) # loss obtained by the arm
+        self.weights[j] *= exp(-eps * loss) # update its weight
+        
+        # We will always keep a copy of the "best" polynomial, that is the one
+        # that gave the smallest loss. And carry on from it.
+        if(loss < self.min_loss):
+            self.min_cooeficients = self.cooficients
+            self.min_loss = loss
+        else:
+            self.cooficients = self.min_cooeficients
+
+        if self.log:
+            formated_dist = ', '.join(list(map(lambda x: '%.3e' % x, distribution)))
+            print(("Information about step:\n\t%d arms\n\thaving these weights: %s"
+                + "\n\tExploration factor: %f\n\tDistribution: %s\n\tExpert Picked: "
+                + "%d\n\tLoss: %f\n\tUsing polynomial: %s")
+                % (n, str(self.weights), eps, formated_dist, j, loss, str(self)))
+
+
+    def train(self, training_length):
+        '''
+        Trains the Bandit for a given number of iterations
+
+        :param training_length: Number of iterations to perform
+        '''
+        for i in range(training_length):
+            if self.log:
+                block_len = (1.0*i/training_length)*60
+                output = "#"*int(block_len)
+                sys.stderr.write("\r%.2f%% [%s] (%d/%d) "
+                        % (100.0*i/training_length, output.ljust(60, ' '), i, training_length))
+            self.step()
+
+
+    def get_polynomial(self):
+        '''
+        Returns a lambda representation of the polynomial
+
+        :return: lambda: \R -> \R
+        '''
+        return lambda x:\
+                sum(map(lambda y: y[1] * x ** y[0],
+                        enumerate(self.cooficients)))
+
+
+    def __repr__(self):
+        '''
+        Outputs a representation of the polynomial
+
+        :return: string
+        '''
+        return ' '.join(map(lambda x:\
+                                ('+ ' if x[1] >= 0 else '- ')
+                                + (str(abs(x[1])) if abs(x[1]) != 1 else '')
+                                + ('x^' + str(x[0]) if x[0] > 0 else ('1' if abs(x[1]) == 1 else '')),
+                            enumerate(self.cooficients)))
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import Graph
 import Colouring
 import CMDPlot
+import Bandit
 
 def beta(t):
     return 1.0/(t**.5+1)
@@ -9,8 +10,11 @@ if __name__ == '__main__':
     # Create a random graph with N = 1000 vertices
     g = Graph.erdos_renyi(1000, 1.87)
     # Create a colouring with q = 750 and a beta function
-    c = Colouring.Colouring(g, 550, beta)
-    # number of iterations
-    iter_num = int(2e5)
-    # plot the results using the poor-man's plot
-    CMDPlot.command_line_plot(c.metropolis(iter_num), x_precision=120, y_precision=60)
+    # c = Colouring.Colouring(g, 550, beta)
+    # # number of iterations
+    # iter_num = int(2e5)
+    # # plot the results using the poor-man's plot
+    # CMDPlot.command_line_plot(c.metropolis(iter_num), x_precision=120, y_precision=60)
+
+    bandit = Bandit.Bandit_beta(g, 350, metropolis_iter=1e4)
+    bandit.train(1000)
