a31b155fc2b775b0edb7e6903a079f312629ca81
George Zakhour <g1995z@gmail.com>
2016-05-17 15:57:16 +0200

    Made a simple command line plotter and a test file
    The plotter can plot data in the command line.
    It's a very simple plotter just for minimal visualization.

---
+++ CMDPlot.py
@@ -0,0 +1,48 @@
+'''
+    This simple module implements a function that can plot data to a command
+    line. Might be faster than waiting for a gui instance to load and plot
+    the data. The plotter is a very crude one.
+'''
+
+
+def command_line_plot(history, x_precision = 1, y_precision=20, x_start=0):
+    """
+    A poor man's plot. Plot data on the command line.
+
+    :param history: An array of numerical values to plot.
+                    The index is ploted on the horizontal axis and the value on
+                    the vertical axis
+    :param x_precision: How many ticks to show on the horizontal axis.
+    :param y_precision: How many ticks to show on the vertical axis.
+    :param x_start: From which point in the history the plotting should start.
+    """
+    whole_max = max(history)
+    whole_min = min(history)
+
+    history = history[x_start:]
+    max_entry = max(history)
+    min_entry = min(history)
+
+    y_step = int((max_entry - min_entry) / y_precision)+1
+    x_step = int(len(history) / x_precision)
+
+    # plot the y axis and the data
+    for y in range(int(min_entry), int(max_entry)+2, y_step)[::-1]:
+        print('%10d |' % y, end='')
+        for i in range(0, len(history), x_step):
+            h = history[i]
+            print('*' if y <= h and h < y+y_step else ' ', end='')
+        print()
+
+    # plot the x axis
+    print(' '*11 + '-'*(x_precision+1))
+    print(' '*12, end='')
+
+    for t in range(0, len(history), x_step):
+        if t/x_step % 10 == 0:
+            label = '%d' % t
+            print(label.ljust(10, ' '), end='')
+
+    print("(+%d)" % x_start)
+    print("min/max y: %d, %d" % (whole_min, whole_max))
+
---
+++ main.py
@@ -0,0 +1,16 @@
+import Graph
+import Colouring
+import CMDPlot
+
+def beta(t):
+    return 1.0/(t**.5+1)
+
+if __name__ == '__main__':
+    # Create a random graph with N = 1000 vertices
+    g = Graph.erdos_renyi(1000, 1.87)
+    # Create a colouring with q = 750 and a beta function
+    c = Colouring.Colouring(g, 550, beta)
+    # number of iterations
+    iter_num = int(2e5)
+    # plot the results using the poor-man's plot
+    CMDPlot.command_line_plot(c.metropolis(iter_num), x_precision=120, y_precision=60)
