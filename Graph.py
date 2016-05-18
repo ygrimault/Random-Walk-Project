7ea5952afa0e652d2f4748eeb733f4e482fdc07f
Sir Isaac Newton <marc.chevalier@ens-lyon.org>
2016-05-18 21:51:09 +0200

    Graph: Deduce the number of vertices from the adjacency matrix

--- Graph.py
+++ Graph.py
@@ -4,16 +4,13 @@ from random import *
 class Graph:
     """ Graph with vertices that can be colored """
 
-    def __init__(self, n, mat):
-        self.n = n
+    def __init__(self, mat):
+        self.n = len(mat)
         self.adjacency = list(map(lambda row: set([i for i, x in enumerate(row) if x == 1]), mat))
-        self.coloring = [0] * n
+        self.coloring = [0] * self.n
 
     def __repr__(self):
-        string = "List of vertices :\n"
-        for vertex in range(self.n):
-            strv = repr(vertex)
-            string += "  " + strv + "\n"
+        string = "Number of vertices: %s" % self.n
         string += "\n" + "List of edges :\n"
         for a in range(self.n):
             for b in self.adjacency[a]:
@@ -31,4 +28,4 @@ def erdos_renyi(n, c):
             r = int(random() < 1.0 * c / n)
             mat[i][j] = r
             mat[j][i] = r
-    return Graph(n, mat)
+    return Graph(mat)
