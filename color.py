300e74e9a9487eaed74cbcff3e954ccaee1be403
Sir Isaac Newton <marc.chevalier@ens-lyon.org>
2016-05-18 22:00:04 +0200

    Add a script to color a graph in a .mat file.
    It does exactly what we will have to do at the competition.

---
+++ color.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+from math import *
+import sys
+import numpy
+import Graph
+import Colouring
+
+try:
+    import scipy.io as si
+except ImportError:
+    print("Make sure you installed scipy on your computer")
+    si = None
+    sys.exit(1)
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print("Usage: ./color file.mat nb_colors")
+        sys.exit(1)
+    mat_file = si.loadmat(sys.argv[1])
+    adj_mat = mat_file['A']
+    g = Graph.Graph(adj_mat)
+    nb_colors = int(sys.argv[2])
+    coloring = Colouring.Colouring(g, nb_colors, lambda n: 100*0.9**floor(n / 1000))
+    h_hist = coloring.metropolis(10000)
+    print(g.n)
+    print(sum(map(sum, g.adjacency)))
+    output = {'X': numpy.array([[e] for e in g.coloring]),
+              'E': numpy.array(coloring.H)}
+    si.savemat('output', output)
+    coloring.plot(h_hist)
