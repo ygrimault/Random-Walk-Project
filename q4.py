aeb3f9ed1c41ad93e26eca48e899dd5692e345e1
Sir Isaac Newton <marc.chevalier@ens-lyon.org>
2016-05-19 14:25:10 +0200

    Multiprocessing !

--- q4.py
+++ q4.py
@@ -3,6 +3,7 @@ from math import *
 import sys
 import graph
 import colouring
+from multiprocessing import Pool
 
 try:
     import matplotlib.pyplot as plt
@@ -19,6 +20,13 @@ except ImportError:
     sys.exit(1)
 
 try:
+    import numpy
+except ImportError:
+    print("Make sure you installed numpy on your computer")
+    numpy = None
+    sys.exit(1)
+
+try:
     import progressbar
 except ImportError:
     progressbar = None
@@ -29,33 +37,34 @@ def plots(hamiltonian_hists):
     :param hamiltonian_hists: list of lists of values to plot with parameters
     """
     for (q_, hamiltonian_hist) in hamiltonian_hists:
-        plt.plot(range(len(hamiltonian_hist)), hamiltonian_hist, label="q=%s" % q_)
+        plt.plot(numpy.linspace(c_min, c_max, nb_points), hamiltonian_hist, label="q=%s" % q_)
 
-    plt.xlabel('Mean connectivity (c)')
+    plt.xlabel('Density (c)')
     plt.ylabel('Optimal hamiltonian found')
-    plt.title('Metropolis Algorithm')
     plt.grid(True)
     plt.legend(frameon=False)
     plt.show()
 
 
+def one_curve(q):
+    hist = []
+    r = numpy.linspace(c_min, c_max, nb_points)
+    if progressbar:
+        r = (progressbar.ProgressBar(redirect_stdout=True))(r)
+    for c in r:
+        g = graph.erdos_renyi(n, c)
+        coloring = colouring.Colouring(g, q, lambda step, b: (1/0.93)*b if step % floor(exp(2*b)+0.5) == 0 else b)
+        (h_hist, min_index, _) = coloring.metropolis(10 ** 5, get_min=True)
+        hist.append(h_hist[min_index])
+    return q, hist
+
 if __name__ == '__main__':
     qs = [3, 5, 7]
     c_min = 0
-    c_max = 30
+    c_max = 50
+    nb_points = 100
     n = 1000
-    hists = []
-
-    for q in qs:
-        hist = []
-        r = range(c_min, c_max+1)
-        if progressbar:
-            r = (progressbar.ProgressBar(redirect_stdout=True))(r)
-        for c in r:
-            g = graph.erdos_renyi(n, c)
-            coloring = colouring.Colouring(g, q, lambda step, b: (1/0.93)*b if step % floor(exp(2*b)+0.5) == 0 else b)
-            (h_hist, min_index, _) = coloring.metropolis(10 ** 5, get_min=True)
-            hist.append(h_hist[min_index])
-        hists.append((q, hist))
+    p = Pool(3)
+    hists = p.map(one_curve, qs)
 
     plots(hists)
