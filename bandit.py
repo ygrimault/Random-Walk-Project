23dd7de9babb9c6096441eaa435cd75c0baf0b62
George Zakhour <g1995z@gmail.com>
2016-05-19 23:08:37 +0200

    Generalized the bandit to accept any function template.
    The main will now attempt to find the best variables for an exponential,
    logarithmic and polynomial beta. These are given as follows:
    
    Exponential template:
    ```beta(t) = T_0 exp(a * floor(t/tau)```
    
    Logarithmic template:
    ```beta(x) = D log(a * floor(x/t))```
    
    Polynomial template: (even degree was chosen so that the polynomial is
    eventually increasing)
    ```beta(t) = ax^6 + bx^5 + cx^4 + dx^3 + ex^2 + fx + g```
    
    The training will be on N=1,000 c=1.8 Erdos-Renyi grayhs, with q=350
    with 1,000,000 iterations and 10,000 training iterations. In the comment
    on this commit you will find the results for 100,000 iterations and
    1,000 training iterations

--- bandit.py
+++ bandit.py
@@ -14,60 +14,65 @@ except ImportError:
 class BanditBeta:
 
     @staticmethod
-    def _lambda_change_ith_coefficient(i, v):
+    def _lambda_change_ith_variable(i, v):
         """
-        Generates a lambda that when invoked on a set of coefficients will
+        Generates a lambda that when invoked on a set of variables will
         increment the i-th one by v
 
-        :param i: the i-th coefficient
-        :param v: the additive constant to add to the i-th coefficient
+        :param i: the i-th variable
+        :param v: the additive constant to add to the i-th variable
 
         :return: lambda : \R^{degree+1} -> \R^{degree+1}
         """
         return lambda arr: \
-            [x + int(j == i)*v for j, x in enumerate(arr)]
+            [x*v for j, x in enumerate(arr)]
 
-    def __init__(self, graph, number_colours, metropolis_iter=1e5, degree=6, log=True):
+    def __init__(self, graph, number_colours, function_template, num_variables,
+            metropolis_iter=1e5, log=True, initial_assignments=[]):
         """
-        Create the bandit instance with a polynomial of a given degree.
-        There will be 2*degree+2 arms, each will either increase or decrease
-        a coefficient with a value of 0.1/i! where i is the i-th power
-        corresponding to that coefficient.
+        Create the bandit instance with a function template and its variables.
+        There will be 2*num_variables arms, each will either increase or decrease
+        a variable by 10%
 
         :param graph: graph on which to do the colouring
         :param number_colours: number of colours to colour the graph
+        :param function_template: a function which given some variables returns a function
+            that given a real returns a real, i.e. a template of the function
         :param metropolis_iter: number of iterations to run the metropolis algorithm on
-        :param degree: the degree of the polynomial
+        :param num_variables: the number of free variables to optimize over
         :param log: whether or not to log the process of learning
         """
         self.graph = graph
         self.number_colours = number_colours
+        self.function_template = function_template
         self.metropolis_iter = metropolis_iter
-        self.log = log  # Whether or not to log the process
-        self.degree = degree
-        self.coefficients = [1] * (degree+1)
+        self.log = log
+        self.num_variables = num_variables
+        self.variables = [1] * num_variables if len(initial_assignments) == 0 else initial_assignments
         self.arms = []
-        self.weights = [1.0] * (2*degree+2)
+        self.weights = [1.0] * (2*num_variables)
+        print(self.variables)
         for i in range(len(self.weights)):
-            v = (-1) ** i * 0.2
-            self.arms.append(self._lambda_change_ith_coefficient(int(i/2), v))
+            v = 1 + (-1) ** i * 0.1
+            self.arms.append(self._lambda_change_ith_variable(int(i/2), v))
 
         self.min_loss = float('inf')  # the minimum loss found so far
-        self.min_coefficients = []  # ^ its corresponding coefficients
+        self.min_variables = []  # ^ its corresponding variables
 
     def loss(self, j):
         """
         Compute the loss produced by choosing the j-th arm.
         The loss of an arm is the following: (We can maybe choose a different one)
         The first time the minimum value is attained / number of iterations.
+        This function runs 5 trials and chooses the median of the losses found
 
         :param j: the index of the arm selected
         :return: l \in [0, 1] (Needed to have a guarantee on the convergence)
         """
-        self.coefficients = self.arms[j](self.coefficients)
-        new_beta = self.get_polynomial()
+        self.variables = self.arms[j](self.variables)
+        new_beta = self.get_function()
         losses = []
-        for _ in range(10):
+        for _ in range(5):
             c = Colouring(self.graph, int(self.number_colours), new_beta)
             history = c.metropolis(int(self.metropolis_iter))
             losses.append(history.index(min(history))/len(history))
@@ -93,21 +98,24 @@ class BanditBeta:
         loss = self.loss(j)  # loss obtained by the arm
         self.weights[j] *= exp(-eps * loss)  # update its weight
 
-        # We will always keep a copy of the "best" polynomial, that is the one
+        # We will always keep a copy of the "best" varialbles, that is the one
         # that gave the smallest loss. And carry on from it.
         if loss < self.min_loss:
-            self.min_coefficients = self.coefficients
+            self.min_variables = self.variables
             self.min_loss = loss
-        # With 99% of the time switch back to the original coefficients
-        elif random.random() > 0.01:
-            self.coefficients = self.min_coefficients
+        # With 99% of the time switch back to the original variables when the
+        # loss is not too much higher than the minimum one found (0.1)
+        elif random.random() > 0.01 and loss - self.min_loss >= 0.1:
+            self.variables = self.min_variables
 
         if self.log:
             formatted_dist = ', '.join(list(map(lambda x: '%.3e' % x, distribution)))
             print(("Information about step:\n\t%d arms\n\thaving these weights: %s" +
                    "\n\tExploration factor: %f\n\tDistribution: %s\n\tExpert Picked: " +
-                   "%d\n\tLoss: %f\n\tOptimal loss: %f\n\tUsing polynomial: %s")
-                  % (n, str(self.weights), eps, formatted_dist, j, loss, self.min_loss, str(self)))
+                   "%d\n\tLoss: %f\n\tOptimal loss: %f\n\tUsing variables: %s\n\t"+
+                   "With minimal variables: %s")
+                   % (n, str(self.weights), eps, formatted_dist, j, loss, self.min_loss,
+                       str(self), str(self.min_variables)))
 
     def train(self, training_length):
         """
@@ -121,31 +129,25 @@ class BanditBeta:
                 self.step()
         else:
             for i in range(training_length):
-                if self.log:
-                    block_len = (1.0*i/training_length)*60
-                    output = "#"*int(block_len)
-                    sys.stderr.write("\r%.2f%% [%s] (%d/%d) "
-                                     % (100.0*i/training_length, output.ljust(60, ' '), i, training_length))
+                block_len = (1.0*i/training_length)*60
+                output = "#"*int(block_len)
+                sys.stderr.write("\r%.2f%% [%s] (%d/%d) "
+                                 % (100.0*i/training_length, output.ljust(60, ' '), i, training_length))
                 self.step()
 
-    def get_polynomial(self):
+    def get_function(self):
         """
-        Returns a lambda representation of the polynomial
+        Returns a function which is the function template provided filled with
+        the variables
 
         :return: lambda: \R -> \R
         """
-        return lambda x, _: \
-            sum(map(lambda y: y[1] * x ** y[0],
-                    enumerate(self.coefficients)))
+        return self.function_template(*self.variables)
 
     def __repr__(self):
         """
-        Outputs a representation of the polynomial
+        Outputs a representation of the variables
 
         :return: string
         """
-        return ' '.join(map(lambda x:
-                            ('+ ' if x[1] >= 0 else '- ') +
-                            (str(abs(x[1])) if abs(x[1]) != 1 else '') +
-                            ('x^' + str(x[0]) if x[0] > 0 else ('1' if abs(x[1]) == 1 else '')),
-                            enumerate(self.coefficients)))
+        return str(self.variables)
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import graph
 import bandit
 import sys
+from math import exp, floor, log
+from random import random
 
 try:
     import scipy.io as si
@@ -9,10 +11,27 @@ except ImportError:
     si = None
     sys.exit(1)
 
+def exp_template(T0, a, tau):
+    def _exp_template(x, y):
+        try:
+            return abs(T0+1e-5) * exp(abs(a+1e-5)*floor(x/abs(tau+1e-5)))
+        except:
+            return 1e10
+    return _exp_template
+
+def log_template(D, a, t):
+    def _log_template(x, y):
+        return abs((D+1e-5)*log((a+1e-5)*floor(x/(t+1e-5)) + 1e-5))
+    return _log_template
+
+def polynomial_template(a, b, c, d, e, f, g):
+    def _polynomial_template(x, y):
+        return a*x**6 + b*x**5 + c*x**4 + d*x**3 + e*x**2 + f*x + g
+    return _polynomial_template
 
 if __name__ == '__main__':
     # Create a random graph with N = 1000 vertices
-    g = graph.erdos_renyi(1000, 1.87)
+    g = graph.erdos_renyi(1000, 1.8)
     # Create a colouring with q = 750 and a beta function
     # c = Colouring.Colouring(g, 550, beta)
     # # number of iterations
@@ -20,5 +39,27 @@ if __name__ == '__main__':
     # # plot the results using the poor-man's plot
     # CMDPlot.command_line_plot(c.metropolis(iter_num), x_precision=120, y_precision=60)
 
-    bandit = bandit.BanditBeta(g, 350, metropolis_iter=1e4)
-    bandit.train(1000)
+    sys.stderr.write(">> Generated Erdos-Renyi graph with N=1,000 and c=1.8\n")
+    sys.stderr.write(">> Trying bandit with exponential template \n")
+    sys.stderr.write(">>>> q = 350 with 1,000,000 iterations with 10,000 training iterations\n")
+    exp_bandit = bandit.BanditBeta(g, 35, exp_template, 3, metropolis_iter=1e6,
+            initial_assignments=[random(), random(), random()], log=True)
+    exp_bandit.train(10000)
+    sys.stderr.write("\n>>>> Optimal variables: %s\n" % exp_bandit.variables)
+    sys.stderr.write(">>>> Optimal loss: %s\n\n" % exp_bandit.min_loss)
+
+    sys.stderr.write(">> Trying bandit with log template \n")
+    sys.stderr.write(">>>> q = 350 with 1,000,000 iterations with 10,000 training iterations\n")
+    log_bandit = bandit.BanditBeta(g, 35, log_template, 3, metropolis_iter=1e6,
+            initial_assignments=[random(), random(), random()], log=True)
+    log_bandit.train(10000)
+    sys.stderr.write("\n>>>> Optimal variables: %s\n" % log_bandit.variables)
+    sys.stderr.write(">>>> Optimal loss: %s\n\n" % log_bandit.min_loss)
+
+    sys.stderr.write(">> Trying bandit with polynomial template \n")
+    sys.stderr.write(">>>> q = 350 with 1,000,000 iterations with 10,000 training iterations\n")
+    pol_bandit = bandit.BanditBeta(g, 35, polynomial_template, 7, metropolis_iter=1e6,
+            initial_assignments=[random(), random(), random(), random(), random(), random(), random()], log=True)
+    pol_bandit.train(10000)
+    sys.stderr.write("\n>>>> Optimal variables: %s\n" % pol_bandit.variables)
+    sys.stderr.write(">>>> Optimal loss: %s\n\n" % pol_bandit.min_loss)
