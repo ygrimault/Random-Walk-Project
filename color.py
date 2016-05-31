793b56e441635eebd3e38442dec75e14bb604109
Sir Isaac Newton <marc.chevalier@ens-lyon.org>
2016-06-01 00:27:49 +0200

    Helm, warp ten. Engage.
    Parallel!!!!!

--- color.py
+++ color.py
@@ -5,6 +5,7 @@ import numpy
 import graph
 import colouring
 import os
+from multiprocessing import Pool
 
 try:
     import matplotlib.pyplot as plt
@@ -32,22 +33,37 @@ def plot(hamiltonian_hist):
     plt.grid(True)
     plt.show()
 
+
+def color_thread(thread_number):
+    g = graph.Graph(adj_mat)
+    coloring = colouring.Colouring(g, nb_colors, lambda step, b: (1/0.93)*b if step % floor(exp(2*b)+0.5) == 0 else b)
+    (h_hist_thread, min_index_thread, opt_coloring_thread) = coloring.metropolis(nb_steps, get_min=True, show_progress=True)
+    print("Thread %d: Minimum energy: %d" % (thread_number, h_hist_thread[min_index_thread]))
+    print("Thread %d: Reached at: %d" % (thread_number, min_index_thread))
+    return h_hist_thread, min_index_thread, opt_coloring_thread, thread_number
+
+
 if __name__ == '__main__':
     if len(sys.argv) < 3:
-        print("Usage: %s input_file.mat nb_colors [steps]" % sys.argv[0])
+        print("Usage: %s input_file.mat nb_colors [steps] [nb_threads]" % sys.argv[0])
         sys.exit(1)
     mat_file = si.loadmat(sys.argv[1])
     adj_mat = mat_file['A']
     n = len(adj_mat)
-    g = graph.Graph(adj_mat)
     nb_colors = int(sys.argv[2])
-    coloring = colouring.Colouring(g, nb_colors, lambda step, b: (1/0.93)*b if step % floor(exp(2*b)+0.5) == 0 else b)
     nb_steps = 10**4
     if len(sys.argv) >= 4:
         nb_steps = int(sys.argv[3])
-    (h_hist, min_index, opt_coloring) = coloring.metropolis(nb_steps, get_min=True, show_progress=True)
-    print("Minimum energy: %d" % h_hist[min_index])
-    print("Reached at: %d" % min_index)
+
+    if len(sys.argv) >= 5:
+        p = Pool(int(sys.argv[4]))
+        results = p.map(color_thread, list(range(int(sys.argv[4]))))
+        h_hist, min_index, opt_coloring, thread = min(results, key=lambda e: (e[0])[e[1]])
+    else:
+        h_hist, min_index, opt_coloring, thread = color_thread(0)
+
+    print("Thread number %d has the best solution." % thread)
+
     output = {'X': numpy.array([[e] for e in opt_coloring]),
               'E': numpy.array(h_hist[min_index])}
     output_filename = 'output_%s_q%d_E%d' % (os.path.splitext(sys.argv[1])[0], nb_colors, h_hist[min_index])
